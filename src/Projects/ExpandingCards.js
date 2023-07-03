8aa19dd0569c03349175df8501f6d761bd950c3a
TONI <zingoo16@gmail.com>
2023-07-04 00:15:27 +0300

    stage is set for creating new projects

--- src/Projects/ExpandingCards.js
+++ src/Projects/ExpandingCards.js
@@ -1,13 +1,18 @@
 import '../App.css'
 // import Container from '../Components/Container'
 import BackBtn from '../Components/Container'
+import { Link } from 'react-router-dom'
 
 function ExpandingCards() {
   return (
     <>
       <div>
-        <BackBtn />
-        <h1>asd</h1>
+        <Link to='/'>
+          <button className='btn btn-dark backBtn'>Go Back</button>
+        </Link>
+        <div className='project'>
+          <h1>Expanding Cards</h1>
+        </div>
       </div>
       {console.log('asd')}
     </>

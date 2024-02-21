0fe8bc81180e14ceb4e51fb34364a0f8e84ac39c
TONI <zingoo16@gmail.com>
2024-02-21 13:00:32 +0200

    added images to gh

--- src/Projects/TestProject.js
+++ src/Projects/TestProject.js
@@ -1,7 +1,21 @@
 import React from 'react'
+import './Styles/TestProject.css';
+
+const IMAGES = ["sandbox/public/TestProjectImages/Section.png",
+"sandbox/public/TestProjectImages/Section-2.png",
+"sandbox/public/TestProjectImages/Section-3.png",
+"sandbox/public/TestProjectImages/Section-4.png"]
 
 function TestProject() {
-  return <div>TestProject</div>
+    return (
+        <div>
+            {IMAGES.map((image, index) => (
+                <img key={index} src={image} alt={`Image ${index}`} />
+            ))}
+        </div>
+    );
 }
+
+  
 
 export default TestProject

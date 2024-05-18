034d688dc2c966d7aba9b3c4aa8a56ae0d38900e
Toni Rajamäki <zingoo16@gmail.com>
2024-05-18 09:23:32 +0300

    added github pages support

--- src/Projects/TestProject.js
+++ src/Projects/TestProject.js
@@ -1,21 +1,23 @@
 import React from 'react'
-import './Styles/TestProject.css';
+import './Styles/TestProject.css'
 
-const IMAGES = ["sandbox/public/TestProjectImages/Section.png",
-"sandbox/public/TestProjectImages/Section-2.png",
-"sandbox/public/TestProjectImages/Section-3.png",
-"sandbox/public/TestProjectImages/Section-4.png"]
+const IMAGES = [
+  'https://github.com/ToniRajamaki/Ezprojects/blob/main/sandbox/public/TestProjectImages/Section-1.png?raw=true',
+  'https://github.com/ToniRajamaki/Ezprojects/blob/main/sandbox/public/TestProjectImages/Section-2.png?raw=true',
+  'https://github.com/ToniRajamaki/Ezprojects/blob/main/sandbox/public/TestProjectImages/Section-3.png?raw=true',
+  'https://github.com/ToniRajamaki/Ezprojects/blob/main/sandbox/public/TestProjectImages/Section.png?raw=true',
+]
 
 function TestProject() {
-    return (
-        <div>
-            {IMAGES.map((image, index) => (
-                <img key={index} src={image} alt={`Image ${index}`} />
-            ))}
+  return (
+    <div>
+      {IMAGES.map((image, index) => (
+        <div className='image-container'>
+          <img key={index} src={image} alt={`Image ${index}`} />
         </div>
-    );
+      ))}
+    </div>
+  )
 }
-
-  
 
 export default TestProject
